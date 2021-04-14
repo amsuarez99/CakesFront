ac34091e743e6d228ee9c1a41ffbda8e9cf36457
Alejandro Hernández López <alexhl1999@hotmail.com>
2021-04-14 18:12:57 -0500

    Added router

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '../views/Home.vue';
+import About from '../views/About.vue';
+import Catalogo from '../views/Catalogo.vue';
 
 const routes = [
     {
@@ -8,12 +10,17 @@ const routes = [
         component: Home
     },
     {
+        path: '/catalogo',
+        name: 'Catalogo',
+        component: Catalogo
+    },
+    {
         path: '/about',
         name: 'About',
         // route level code-splitting
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+        component: About
     }
 ]
 
---
+++ src/views/Catalogo.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h2>Mi Catalogo</h2>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
