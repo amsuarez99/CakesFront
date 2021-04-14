e9ae7c1abeb8aad9971a01906cf47345a942ddfd
Alejandro Hernández López <alexhl1999@hotmail.com>
2021-04-14 17:59:31 -0500

    Added router

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,10 @@
 <template>
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view/>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
 <style>
 #app {
@@ -22,8 +15,16 @@ export default {
   color: #2c3e50;
 }
 
-* {
-  margin: 0;
-  padding: 0;
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "@/components/HelloWorld";
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
