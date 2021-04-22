b4a7d3aaa33413a57e36f987e722b31acb7234b0
Alejandro Hernández López <alexhl1999@hotmail.com>
2021-04-22 15:04:12 -0500

    Added bot

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -24,7 +24,7 @@
       </div>
     </div>
 </div>
-  
+
 
 <!-- Intro -->
 <div class="container-fluid my-4">
@@ -32,9 +32,9 @@
     <div class="col-md-4 photo-container">
       <span class="photo rounded-circle mx-auto mb-4"></span>
     </div>
-    <div class="col-md-8 d-flex"> 
+    <div class="col-md-8 d-flex">
       <div class="d-flex flex-row align-items-center">
-        <div class="p-5 intro-text">  
+        <div class="p-5 intro-text">
           <h2 class="display-6">Soy Cecilia Ponce</h2>
           <p class="">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut laboreLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore</p>
         </div>
@@ -46,9 +46,9 @@
 <!-- Reel -->
 <div class="container-fluid reel">
   <div class="row">
-    <div class="col-md-6 d-flex"> 
+    <div class="col-md-6 d-flex">
       <div class="d-flex flex-row align-items-center">
-        <div class="p-5 reel-text">  
+        <div class="p-5 reel-text">
           <h2 class="display-6 d-block">Un poco de mi cocina</h2>
           <h2 class="display-7 d-block">Horneado Casero</h2>
           <p class="d-block">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus sed fuga delectus dolores laudantium magni laborum doloremque dolore, ab explicabo sint exercitationem iure molestias dignissimos, dolorem quidem eius ad consequatur.</p>
@@ -65,7 +65,7 @@
 <div class="container-fluid catalog">
   <div class="row my-5 my-md-0">
     <span class="col-md-6 d-block box-img"></span>
-    <div class="col-md-6"> 
+    <div class="col-md-6">
       <div class="d-md-flex align-items-center">
         <div class="sales p-5">
           <h2 class="display-7">Cajas de Desayuno</h2>
@@ -76,7 +76,7 @@
     </div>
   </div>
   <div class="row my-5 my-md-0">
-    <div class="col-md-6"> 
+    <div class="col-md-6">
       <div class="d-md-flex align-items-center">
         <div class="sales p-5">
           <h2 class="display-7 text-center">Cajas de Desayuno</h2>
@@ -99,9 +99,23 @@ export default {
   },
   components: {
     ButtonComponent
+  },
+  methods: {
+  },
+  mounted (){
+    (function(d, m){
+      var kommunicateSettings = {"appId":"1731a868491b9a64cc451a3f8887e7400","popupWidget":true,"automaticChatOpenOnNavigation":true};
+      var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+      s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+      var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+      window.kommunicate = m; m._globals = kommunicateSettings;
+    })(document, window.kommunicate || {});
+  },
+  data: function(){
   }
 }
 </script>
+
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
@@ -172,7 +186,7 @@ p {
   background-color: #C4C4C4;
   color: black;
   font-weight: 500;
-} 
+}
 
 .media {
   background-color: #4E4E4E;
@@ -202,6 +216,6 @@ p {
 }
 
 .box-img {
-  background-color:#4E4E4E; 
+  background-color:#4E4E4E;
 }
 </style>
