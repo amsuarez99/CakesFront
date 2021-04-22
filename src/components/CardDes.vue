4341a1611fe44894cd4a1831abfda9788d9a7a20
raintics <cristian97acosta@gmail.com>
2021-04-22 18:14:57 -0500

    componentes cards

---
+++ src/components/CardDes.vue
@@ -0,0 +1,17 @@
+<div id="app">
+  <v-app id="inspire">
+    <v-card flat outlined class="mx-auto rounded-xl" max-width="344">
+      <v-list-item three-line>
+        <v-list-item-avatar size="80" color="grey darken-3" rounded-circle></v-list-item-avatar>
+        <v-list-item-content>
+          <v-list-item-title class="headline mb-1">
+            Nombre Producto
+          </v-list-item-title>
+          <v-list-item-subtitle>Insertar descripcion del producto aqui</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+      <v-card-actions>
+      </v-card-actions>
+    </v-card>
+  </v-app>
+</div>
